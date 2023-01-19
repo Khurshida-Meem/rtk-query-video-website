4bc685a1a6ed07577eddb78b31d3656d4c3f2df0
Khurshida Meem <meemrumi59@gmail.com>
2023-01-19 17:28:44 +0600

    added some video content

--- src/components/pages/Video.js
+++ src/components/pages/Video.js
@@ -1,8 +1,27 @@
+import { useParams } from "react-router-dom";
+import { useGetVideoQuery } from "../../features/api/apiSlice";
+import Error from "../ui/Error";
+import DescriptionLoader from "../ui/loaders/DescriptionLoader";
+import RelatedVideoLoader from "../ui/loaders/RelatedVideoLoader";
 import Description from "../video/Description";
 import Player from "../video/Player";
 import RelatedVideos from "../video/related/RelatedVideos";
 
 export default function Video() {
+
+    const {videoId} = useParams();
+
+    const {data:video, isLoading, isError} = useGetVideoQuery(videoId);
+
+    let content = null;
+
+    if(isLoading){
+        content = <><RelatedVideoLoader /><DescriptionLoader /> </>;
+    }
+    if(!isLoading && isError) {
+        content = <Error message="" />
+    }
+
     return (
         <section className="pt-6 pb-20 min-h-[calc(100vh_-_157px)]">
             <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
