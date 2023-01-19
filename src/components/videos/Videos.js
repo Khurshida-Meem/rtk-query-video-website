fa04850aa3480f3f34373ca55e3ce0f704624e74
Khurshida Meem <meemrumi59@gmail.com>
2023-01-19 17:20:06 +0600

    initial commit

---
+++ src/components/videos/Videos.js
@@ -0,0 +1,32 @@
+import { useGetVideosQuery } from "../../features/api/apiSlice";
+import Error from "../ui/Error";
+import VideoLoader from "../ui/loaders/VideoLoader";
+import Video from "./Video";
+
+export default function Videos() {
+  const { data: videos, isLoading, isError } = useGetVideosQuery();
+
+  let content = null;
+
+  if (isLoading) {
+    content = (
+      <>
+        <VideoLoader />
+      </>
+    );
+  }
+
+  if (!isLoading && isError) {
+    content = <Error message="An error occured" />;
+  }
+
+  if (!isLoading && !isError && videos?.length === 0) {
+    content = <Error message="No Videos Found" />;
+  }
+
+  if (!isLoading && !isError && videos?.length > 0) {
+    content = videos?.map((video) => <Video key={video.id} video={video} />);
+  }
+
+  return content;
+}
