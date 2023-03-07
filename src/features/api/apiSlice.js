ff239be044524ac4d03b9946ddd6c9f0b9b69e79
Khurshida Meem <meemrumi59@gmail.com>
2023-03-07 17:15:48 +0600

    added video edit

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -5,24 +5,52 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:9000",
   }),
+  tagTypes: ["Videos", "Video", "RelatedVideo"],
   endpoints: (builder) => ({
     getVideos: builder.query({
       query: () => "/videos",
       keepUnusedDataFor: 600,
+      providesTags: ["Videos"],
     }),
     getVideo: builder.query({
       query: (videoId) => `videos/${videoId}`,
     }),
     // videos?title_like=css&title_like=tailwind&_limit=4
     getRelatedVideos: builder.query({
-      query: ({id, title}) => {
+      query: ({ id, title }) => {
         const tags = title.split(" ");
-        const likes = tags.map(tag => `title_like=${tag}`);
+        const likes = tags.map((tag) => `title_like=${tag}`);
         const queryString = `/videos?${likes.join("&")}&_limit=4`;
         return queryString;
       },
     }),
+    addVideo: builder.mutation({
+      query: (data) => ({
+        url: "/videos",
+        method: "POST",
+        body: data,
+      }),
+      invalidatesTags: ["Videos"],
+    }),
+    editVideo: builder.mutation({
+      query: ({id, data}) => ({
+        url: `/videos/${id}`,
+        method: "PATCH",
+        body: data,
+      }),
+      invalidatesTags: (result, error, arg) => [
+        "Videos",
+        { type: "Video", id: arg.id },
+        { type: "RelatedVideo", id: arg?.id },
+    ],
+    }),
   }),
 });
 
-export const { useGetVideosQuery, useGetVideoQuery, useGetRelatedVideosQuery } = apiSlice;
+export const {
+  useGetVideosQuery,
+  useGetVideoQuery,
+  useGetRelatedVideosQuery,
+  useAddVideoMutation,
+  useEditVideoMutation,
+} = apiSlice;
